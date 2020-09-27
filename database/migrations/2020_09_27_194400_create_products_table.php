08f7c24a05516d29cf3c9d497c8e19a5b12dab36
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2020-09-27 19:42:04 -0400

    Make image nullable in the database

--- database/migrations/2020_09_27_194400_create_products_table.php
+++ database/migrations/2020_09_27_194400_create_products_table.php
@@ -19,7 +19,7 @@ class CreateProductsTable extends Migration
             $table->string('name');
             $table->string('description');
             $table->integer('price');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
